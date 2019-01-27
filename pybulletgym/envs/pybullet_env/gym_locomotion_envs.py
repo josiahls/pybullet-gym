8ce9412a9eced6d1cf0e723798eca374288a3e64
Josiah Laivins <jokellum@northstate.net>
2019-01-27 15:52:52 -0500

    Ok, so the the loadMJCF is missing a ton of materials, has import issues ect. The environment is ugly. I want to switch to URDF since it is better supported by bullet3

---
+++ pybulletgym/envs/pybullet_env/gym_locomotion_envs.py
@@ -0,0 +1,70 @@
+import numpy as np
+
+from .scene_pick_and_place import PickAndPlaceScene
+from .env_bases import BaseBulletEnv
+from .robot_locomotors import Fetch
+
+
+class FetchPickAndPlaceEnv(BaseBulletEnv):
+    def __init__(self):
+        self.robot = Fetch()
+        BaseBulletEnv.__init__(self, self.robot)
+
+        self.joints_at_limit_cost = -0.1
+
+    # def create_single_player_scene(self, bullet_client):
+    #     self.stadium_scene = StadiumScene(bullet_client, gravity=9.8, timestep=0.0165 / 4, frame_skip=4)
+    #     return self.stadium_scene
+
+    def create_single_player_scene(self, bullet_client):
+        self.pick_and_place_scene = PickAndPlaceScene(bullet_client, gravity=9.8, timestep=0.0165 / 4, frame_skip=4)
+        return self.pick_and_place_scene
+
+    def step(self, a):
+        if not self.scene.multiplayer:  # if multiplayer, action first applied to all robots, then global step() called, then _step() for all robots with the same actions
+            self.robot.apply_action(a)
+            self.scene.global_step()
+
+        state = self.robot.calc_state()  # also calculates self.joints_at_limit
+
+        # alive = float(self.robot.alive_bonus(state[0]+self.robot.initial_z, self.robot.body_rpy[1]))   # state[0] is body height above ground, body_rpy[1] is pitch
+        alive = 1  # For no, the robot will always be alive
+        done = alive < 0
+        if not np.isfinite(state).all():
+            print("~INF~", state)
+            done = True
+
+        potential_old = self.potential
+        self.potential = self.robot.calc_potential()
+        progress = float(self.potential - potential_old)
+
+        # electricity_cost  = self.electricity_cost  * float(np.abs(a*self.robot.joint_speeds).mean())  # let's assume we have DC motor with controller, and reverse current braking
+        # electricity_cost += self.stall_torque_cost * float(np.square(a).mean())
+
+        joints_at_limit_cost = float(self.joints_at_limit_cost * self.robot.joints_at_limit)
+        debugmode = 0
+        if debugmode:
+            print("alive=")
+            print(alive)
+            print("progress")
+            print(progress)
+            # print("electricity_cost")
+            # print(electricity_cost)
+            print("joints_at_limit_cost")
+            print(joints_at_limit_cost)
+
+        self.rewards = [
+            alive,
+            progress,
+            # electricity_cost,
+            joints_at_limit_cost,
+        ]
+        if debugmode:
+            print("rewards=")
+            print(self.rewards)
+            print("sum rewards")
+            print(sum(self.rewards))
+        self.HUD(state, a, done)
+        self.reward += sum(self.rewards)
+
+        return state, sum(self.rewards), bool(done), {}
---
+++ pybulletgym/envs/pybullet_env/robot_locomotors.py
@@ -0,0 +1,106 @@
+import numpy as np
+from pybullet_envs.bullet.bullet_client import BulletClient
+
+from pybulletgym.envs.mujoco.robot_bases import XmlBasedRobot, MJCFBasedRobot
+
+
+class MobileManipulatorBase(XmlBasedRobot):
+
+    def __init__(self, power):
+        self.power = power
+        self.camera_x = 0
+        self.start_pos_x, self.start_pos_y, self.start_pos_z = 0, 0, 0
+        self.manipulator_target_x = 1e3  # kilometers away
+        self.manipulator_target_y = 0
+        self.body_xyz = [0, 0, 0]
+
+    def robot_specific_reset(self, bullet_client: BulletClient):
+        self._p = bullet_client
+        for j in self.ordered_joints:
+            j.reset_current_position(self.np_random.uniform(low=-0.1, high=0.1), 0)
+
+        self.scene.actor_introduce(self)
+        self.initial_z = None
+
+    def apply_action(self, a):
+        assert (np.isfinite(a).all())
+        i = 0
+        for n, j in enumerate(self.ordered_joints):
+            if j.power_coef != 0:  # in case the ignored joints are added, they have 0 power
+                j.set_motor_torque(self.power * j.power_coef * float(np.clip(a[n - i], -1, +1)))
+            else:
+                i += 1
+
+    def calc_state(self):
+        qpos = np.array([j.get_position() for j in self.ordered_joints], dtype=np.float32).flatten()  # shape (6,)
+        qvel = np.array([j.get_velocity() for j in self.ordered_joints], dtype=np.float32).flatten()  # shape (6,)
+
+        return np.concatenate([
+            qpos.flat[1:],  # self.sim.data.qpos.flat[1:],
+            np.clip(qvel, -10, 10).flat  # self.sim.data.qvel.flat,
+        ])
+
+    def calc_potential(self):
+        # progress in potential field is speed*dt, typical speed is about 2-3 meter per second, this potential will change 2-3 per frame (not per second),
+        # all rewards have rew/frame units and close to 1.0
+        debugmode = 0
+        if debugmode:
+            print("calc_potential: self.walk_target_dist")
+            print(self.walk_target_dist)
+            print("self.scene.dt")
+            print(self.scene.dt)
+            print("self.scene.frame_skip")
+            print(self.scene.frame_skip)
+            print("self.scene.timestep")
+            print(self.scene.timestep)
+        return - self.walk_target_dist / self.scene.dt
+
+
+class Fetch(MobileManipulatorBase, MJCFBasedRobot):
+
+    def __init__(self):
+        MobileManipulatorBase.__init__(self, power=0.75)
+        MJCFBasedRobot.__init__(self, "fetch/main.xml", "base_link", action_dim=15, obs_dim=11,
+                                add_ignored_joints=True)
+
+        self.pos_after = 0
+
+    def calc_state(self):
+        qpos = np.array([j.get_position() for j in self.ordered_joints], dtype=np.float32).flatten()  # shape (6,)
+        qvel = np.array([j.get_velocity() for j in self.ordered_joints], dtype=np.float32).flatten()  # shape (6,)
+
+        j = np.array([j.current_relative_position() for j in self.ordered_joints], dtype=np.float32).flatten()
+        # even elements [0::2] position, scaled to -1..+1 between limits
+        # odd elements  [1::2] angular speed, scaled to show -1..+1
+        self.joint_speeds = j[1::2]
+        self.joints_at_limit = np.count_nonzero(np.abs(j[0::2]) > 0.99)
+
+        return np.concatenate([
+            qpos.flat[1:],  # self.sim.data.qpos.flat[1:],
+            np.clip(qvel, -10, 10).flat  # self.sim.data.qvel.flat,
+        ])
+
+    def calc_potential(self):
+        # progress in potential field is speed*dt, typical speed is about 2-3 meter per second, this potential will change 2-3 per frame (not per second),
+        # all rewards have rew/frame units and close to 1.0
+        pos_before = self.pos_after
+        self.pos_after = self.robot_body.get_pose()[0]
+        debugmode = 0
+        if debugmode:
+            print("calc_potential: self.walk_target_dist")
+            print(self.walk_target_dist)
+            print("self.scene.dt")
+            print(self.scene.dt)
+            print("self.scene.frame_skip")
+            print(self.scene.frame_skip)
+            print("self.scene.timestep")
+            print(self.scene.timestep)
+        return (self.pos_after - pos_before) / self.scene.dt
+
+    def robot_specific_reset(self, bullet_client):
+        self._p = bullet_client
+        self.scene.actor_introduce(self)
+        self.initial_z = None
+
+    def alive_bonus(self, z, pitch):
+        return +2 if z > 0.78 else -1  # 2 here because 17 joints produce a lot of electricity cost just from policy noise, living must be better than dying
